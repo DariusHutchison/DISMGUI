141d86dc8bdbba91db48abd14bc94327747d9efa
Darius Hutchison <darius.hutchison@protonmail.com>
2025-04-28 18:12:02 -0500

    Removed old .NET Framework Versions. Upgraded current version to .NET Core 9.0. Updated .gitignore for new folder structure.

---
+++ DISM GUI/My Project/AssemblyInfo.vb
@@ -0,0 +1,9 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("c3f3511e-706d-408a-b416-c9ab829edbfe")> 
