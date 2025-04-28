6e927300091c60e43348256bd9860e11a7ded99e
Darius Hutchison <darius.hutchison@protonmail.com>
2025-04-28 18:03:25 -0500

    Revert "Removed old .NET Framework Versions. Upgraded current version to .NET Core 9.0."
    This reverts commit 291d80c7f9557402076989c666ec510e145fd52c.

--- DISM GUI/My Project/AssemblyInfo.vb
+++
@@ -1,9 +0,0 @@
-﻿Imports System
-Imports System.Reflection
-Imports System.Runtime.InteropServices
-<Assembly: AssemblyTrademark("")> 
-
-<Assembly: ComVisible(False)>
-
-'The following GUID is for the ID of the typelib if this project is exposed to COM
-<Assembly: Guid("c3f3511e-706d-408a-b416-c9ab829edbfe")> 
